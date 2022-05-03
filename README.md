04c1a8360fde26c49f84141c8b15df778ab556d1
ioplker <panswitcher@yandex.ru>
2022-05-03 17:39:41 +0700

    :sparkles: :memo: Binded config to logic; added explanation of breaks to README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # uspokoysa
 Dead simple Nim app for making timebreaks
+
+## How it works
+Suppose you are using such config (numbers correlate to dots and dashes on the schema below):
+```
+shortBreakDuration: 1    # in seconds
+shortBreakInterval: 6    # in minutes
+longBreakDuration: 3     # in minutes
+longBreakInterval: 3     # in short breaks count
+```
+
+```
+------<.>------<.>------<.>------<...>------<.>------<.>------<.>------<...>
+_______|        |        |         |         |
+short break #1  |        |         |         |
+                |        |         |         |
+________________|        |         |         |
+short break #2           |         |         |
+                         |         |         |
+_________________________|         |         |
+short break #3                     |         |
+                                   |         |
+___________________________________|         |
+long break                                   |
+                                             |
+_____________________________________________|
+short break #1 (again)
+and so on...
+```
--- src/uspokoysa.nim
+++ src/uspokoysa.nim
@@ -1,3 +1,5 @@
+# See codestyle conventions at
+# https://gist.github.com/ioplker/6bdf0de65514499b8dc9c81cfbbb941e
 import std/[os, parseutils]
 import strutils
 
@@ -7,18 +9,18 @@ import nigui
 #[ Types ]#
 type
   Settings* = object
-    shortBreakDuration*: int   # in seconds
-    shortBreakInterval*: int   # in minutes
-    longBreakDuration*: int    # in minutes
-    longBreakInterval*: int    # in short breaks count
+    shortBreakDuration*: int     # in seconds
+    shortBreakInterval*: float   # in minutes
+    longBreakDuration*: float    # in minutes
+    longBreakInterval*: int      # in short breaks count
     notificationCmd*: string
     screenLockCmd*: string
 
   Status* = object
     settings*: Settings
     shortBreaksLeft*: int
-    restCounter*: int    # how many seconds until current time break ends
-    workCounter*: int    # how many seconds until next time break starts
+    restCounter*: int    # how many seconds until current time break ends, `-1` when break is already ended
+    workCounter*: int    # how many seconds until next time break starts, `-1` when break is already started
 
   Face* = ref object
     text*: string
@@ -38,7 +40,7 @@ const
 
 
 #[ Globals ]#
-var globalStatus: Status
+var status: Status
 var counter = 4
 var faceLabel: Label
 var timeLabel: Label
@@ -48,10 +50,10 @@ var timer: Timer
 
 #[ Methods ]#
 proc main()
+proc initSettings(): Settings
 proc initFaces()
 proc initGui()
 
-proc getSettings(): Settings
 
 proc workTimeLoop(event: TimerEvent)
 proc restTimeLoop(event: TimerEvent)
@@ -67,9 +69,85 @@ proc quitApp()
 
 
 proc main() =
-  let settings = getSettings()
+  var settings = initSettings()
   initFaces()
   initGui()
+
+
+proc initSettings(): Settings =
+  var settings: Settings = DefaultSettings
+
+  if fileExists(expandTilde("~" / ".uspokoysarc")):
+    echo "Using config: " & expandTilde("~" / ".uspokoysarc")
+    const configContents = readFile(expandTilde("~" / ".uspokoysarc"))
+
+    var rawConfigLines = configContents.splitLines()
+    var configLines: seq[string]
+
+    # Filtering out comments and empty lines
+    for configString in rawConfigLines:
+      let handledPair = configString.split("#")[0]
+      if handledPair.len > 0:
+        configLines.add(handledPair)
+
+    # Parsing strings with `key: value` pattern
+    for configString in configLines:
+      let pair = configString.split(":")
+      let key = pair[0].strip()
+      let value = pair[1].strip()
+
+      case key:
+        of "shortBreakDuration":
+          var parsedValue: int
+
+          if parseInt(value, parsedValue, 0) == 0 or parsedValue == 0:
+            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
+          else:
+            settings.shortBreakDuration = parsedValue
+
+        of "shortBreakInterval":
+          let parsedValue = parseFloat(value)
+
+          if parsedValue == 0:
+            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
+          else:
+            settings.shortBreakInterval = parsedValue
+
+        of "longBreakDuration":
+          let parsedValue = parseFloat(value)
+
+          if parsedValue == 0:
+            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
+          else:
+            settings.longBreakDuration = parsedValue
+
+        of "longBreakInterval":
+          var parsedValue: int
+
+          if parseInt(value, parsedValue, 0) == 0 or parsedValue == 0:
+            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
+          else:
+            settings.longBreakInterval = parsedValue
+
+        of "notificationCmd":
+          if value.len == 0:
+            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
+          else:
+            settings.notificationCmd = value
+
+        of "screenLockCmd":
+          if value.len == 0:
+            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
+          else:
+            settings.screenLockCmd = value
+
+        else:
+          raise newException(IOError, "Error! Unexpected config parameter: " & key)
+
+  status.settings = settings
+  status.shortBreaksLeft = settings.longBreakInterval
+  status.restCounter = -1
+  status.workCounter = int(settings.shortBreakInterval * 60)    # converting minutes to seconds
 
 
 proc initFaces() =
@@ -123,121 +201,41 @@ proc initGui() =
   app.run()
 
 
-proc getSettings(): Settings =
-  var settings: Settings
-
-  if fileExists(expandTilde("~" / ".uspokoysarc")):
-    echo "Using config: " & expandTilde("~" / ".uspokoysarc")
-    const configContents = readFile(expandTilde("~" / ".uspokoysarc"))
-
-    var rawConfigLines = configContents.splitLines()
-    var configLines: seq[string]
-
-    # Filtering out comments and empty lines
-    for configString in rawConfigLines:
-      let handledPair = configString.split("#")[0]
-      if handledPair.len > 0:
-        configLines.add(handledPair)
-
-    # Parsing strings with `key: value`
-    for configString in configLines:
-      let pair = configString.split(":")
-      let key = pair[0].strip()
-      let value = pair[1].strip()
-
-      case key:
-        of "shortBreakDuration":
-          var parsedValue: int
-
-          if parseInt(value, parsedValue, 0) == 0:
-            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
-          else:
-            settings.shortBreakDuration = parsedValue
-
-        of "shortBreakInterval":
-          var parsedValue: int
-
-          if parseInt(value, parsedValue, 0) == 0:
-            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
-          else:
-            settings.shortBreakInterval = parsedValue
-
-        of "longBreakDuration":
-          var parsedValue: int
-
-          if parseInt(value, parsedValue, 0) == 0:
-            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
-          else:
-            settings.longBreakDuration = parsedValue
-
-        of "longBreakInterval":
-          var parsedValue: int
-
-          if parseInt(value, parsedValue, 0) == 0:
-            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
-          else:
-            settings.longBreakInterval = parsedValue
-
-        of "notificationCmd":
-          if value.len == 0:
-            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
-          else:
-            settings.notificationCmd = value
-
-        of "screenLockCmd":
-          if value.len == 0:
-            raise newException(IOError, "Error! Unexpected config parameter value in string: " & configString)
-          else:
-            settings.screenLockCmd = value
-
-        else:
-          raise newException(IOError, "Error! Unexpected config parameter: " & key)
-
-
-    settings.shortBreakDuration =
-      if settings.shortBreakDuration == 0: DefaultSettings.shortBreakDuration
-      else: settings.shortBreakDuration
-
-    settings.shortBreakInterval =
-      if settings.shortBreakInterval == 0: DefaultSettings.shortBreakInterval
-      else: settings.shortBreakInterval
-
-    settings.longBreakDuration =
-      if settings.longBreakDuration == 0: DefaultSettings.longBreakDuration
-      else: settings.longBreakDuration
-
-    settings.longBreakInterval =
-      if settings.longBreakInterval == 0: DefaultSettings.longBreakInterval
-      else: settings.longBreakInterval
-
-    settings
-
-  else:
-    DefaultSettings
-
-
 proc workTimeLoop(event: TimerEvent) =
-  if counter == 1:
+  if status.workCounter == 1:
     timer.stop()
-    counter = 7
+
+    status.workCounter = -1
+
+    if status.shortBreaksLeft == 0:
+      status.shortBreaksLeft = status.settings.longBreakInterval
+      status.restCounter = int(status.settings.longBreakDuration * 60)
+    else:
+      status.shortBreaksLeft -= 1
+      status.restCounter = status.settings.shortBreakDuration
+
     timer = startRepeatingTimer(1000, restTimeLoop)
   else:
-    counter -= 1
+    status.workCounter -= 1
 
 
 proc restTimeLoop(event: TimerEvent) =
   face = face.next
   faceLabel.text = face.text
 
-  if counter == 1:
+  if status.restCounter == 1:
     timer.stop()
-    counter = 4
+
+    status.restCounter = -1
+    status.workCounter = int(status.settings.shortBreakInterval * 60)
+
     faceLabel.text = ""
     timeLabel.text = "..."
+
     timer = startRepeatingTimer(1000, workTimeLoop)
   else:
-    counter -= 1
-    timeLabel.text = $counter
+    status.restCounter -= 1
+    timeLabel.text = $status.restCounter
 
 
 proc showTimeBreakNotification() =
